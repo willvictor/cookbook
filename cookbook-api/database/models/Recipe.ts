4b67bb2efa4054047019ed548f3e309e55fadac6
Will Victor <will.victor10@gmail.com>
2020-05-03 18:04:14 -0400

    Update the entire backend to use typescript

---
+++ cookbook-api/database/models/Recipe.ts
@@ -0,0 +1,74 @@
+import {Table, Column, Model, BelongsTo, CreatedAt, UpdatedAt, ForeignKey} from 'sequelize-typescript';
+import {DataTypes} from 'sequelize';
+import {User} from './User';
+
+@Table({
+    tableName: 'recipes',
+    timestamps: true
+})
+export class Recipe extends Model<Recipe> {
+ 
+    @Column({
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    field: "recipe_id"
+    })
+    recipeId: number;
+
+    @ForeignKey(() => User)
+    @Column({
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: "user_id"
+    })
+    userId: number;
+
+    @Column({
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "name"
+    })
+    name: string;
+
+    @Column({
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "ingredients"
+    })
+    ingredients: string;
+
+    @Column({
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "directions"
+    })
+    directions: string;
+
+    @Column({
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "image_url"
+    })
+    imageUrl: string;
+
+    @CreatedAt
+    @Column({ 
+        type: DataTypes.DATE, 
+        defaultValue: DataTypes.NOW,
+        field: "date_created"
+    })
+    dateCreated: Date;
+
+    @UpdatedAt
+    @Column({ 
+        type: DataTypes.DATE, 
+        allowNull: true,
+        field: "date_updated"
+    })
+    dateUpdated: Date;
+ 
+
+    @BelongsTo(() => User)
+    creator: User;
+}
--- cookbook-api/server/GraphQLSchema.ts
+++ cookbook-api/server/GraphQLSchema.ts
@@ -3,16 +3,19 @@ import { GraphQLObjectType,
     GraphQLInt,
     GraphQLList,
     GraphQLString } from 'graphql';
-import { resolver } from "graphql-sequelize";
-const models = require("../database-migrations/models");
-const {OAuth2Client} = require('google-auth-library');
+import {getSequelizeInstance} from '../database/SequelizeFactory';
+import { User } from '../database/models/User';
+import { Recipe } from '../database/models/Recipe';
+import {OAuth2Client} from 'google-auth-library';
 
 
+const sequelize = getSequelizeInstance();
 
 const createSchema = () => {
     const UserType = new GraphQLObjectType({
         name: 'User',
         fields: () => ({
+            
             firstName: {type: GraphQLString},
             lastName: {type: GraphQLString},
             email: {type: GraphQLString},
@@ -29,10 +32,7 @@ const createSchema = () => {
             ingredients: {type: GraphQLString},
             directions: {type: GraphQLString},
             imageUrl: {type: GraphQLString},
-            creator: { 
-                type: UserType,
-                resolve: resolver(models.Recipe.User)
-            }
+            creator: { type: UserType }
         })
     });
         
@@ -44,11 +44,15 @@ const createSchema = () => {
                 args: {
                     recipeId: {type: GraphQLInt}
                 },
-                resolve: resolver(models.Recipe)
+                resolve: async (root, args) => {
+                    return await Recipe.findByPk(args.recipeId, {include: [User]});
+                }
             }, 
             recipes: {
                 type: new GraphQLList(RecipeType),
-                resolve: resolver(models.Recipe)
+                resolve: async (root, args) => {
+                    return await Recipe.findAll();
+                }
             }
         })
     });
@@ -66,7 +70,7 @@ const createSchema = () => {
                     userId: {type: GraphQLInt}
                 },
                 resolve: async (root, args) => {
-                    const newRecipe = models.Recipe.build({
+                    const newRecipe = Recipe.build({
                         name: args.name,
                         ingredients: args.ingredients,
                         directions: args.directions,
@@ -88,26 +92,17 @@ const createSchema = () => {
                         audience: "984941479252-maabsnngi084tun89leu7ts4otp1jldo.apps.googleusercontent.com",
                     });
                     const payload = ticket.getPayload();
-                    const existingUsers = await models.User.findAll({
-                        where: {
-                            googleSubId: payload['sub']
-                        }
-                    });
-                    let user;
-                    if (existingUsers.length > 0){
-                        user = existingUsers[0];
-                    }
-                    else{
-                        const newUser = models.User.build({
+                    if (!payload) throw "payload should not be null";
+                    const [user, created] = await User.findOrCreate({
+                        where: { googleSubId: payload['sub'] },
+                        defaults: {
                             firstName: payload['given_name'],
                             lastName: payload['family_name'],
                             email: payload['email'],
                             imageUrl: payload['picture'],
                             googleSubId: payload['sub']
-                        });
-                        await newUser.save();
-                        user = newUser;
-                    }
+                        }
+                    });
                     root.session.isAuthenticated = true;
                     root.session.userId = user.userId;
                     return user;
