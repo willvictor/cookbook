0be1fea26d8a9009ab0f734c9ca28224ef6e90c2
Will Victor <will.victor10@gmail.com>
2020-05-09 18:31:38 -0400

    Update circle ci and connection to DB

--- cookbook-api/database/SequelizeFactory.ts
+++ cookbook-api/database/SequelizeFactory.ts
@@ -4,6 +4,7 @@ export const getSequelizeInstance = () => new Sequelize({
   database: 'cookbook',
   dialect: 'postgres',
   username: 'postgres',
-  password: '',
+  password: process.env.CLOUD_SQL_DB_PWD || '',
+  host: process.env.CLOUD_SQL_HOST || 'localhost:5432',
   models: [__dirname + '/models']
 });
