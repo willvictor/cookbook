1a90bd92cb181b1f74469df8384d8b2476dcf8e1
Will Victor <will.victor10@gmail.com>
2020-06-06 16:06:57 -0400

    remove unused importS

--- cookbook-front-end/src/Components/Recipes.tsx
+++ cookbook-front-end/src/Components/Recipes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import {Card, CardHeader, CardActions, CircularProgress, CardMedia, GridList, GridListTile, ListSubheader, GridListTileBar, IconButton} from '@material-ui/core';
+import {CircularProgress, GridList, GridListTile, GridListTileBar} from '@material-ui/core';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import Error from './Error';
